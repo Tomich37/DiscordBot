7a94104b1cd81ef67d6c8dde33134a0bacb6eec3
Tomich37 <Tomich7372@gmail.com>
2023-12-12 17:51:00 +0700

    Добавлено удаление ролей

--- app/modules/commands.py
+++ app/modules/commands.py
@@ -13,36 +13,76 @@ class SlashCommands(commands.Cog):
     async def ping(self, inter):
         await inter.response.send_message("Понг!")
 
+    roleMenegment = commands.option_enum({"Назначить роль": "add", "Снять роль": "take"})  
     @commands.slash_command(
-        name="getrole",
-        description="Назначить роль",
+        name="role",
+        description="Назначить/снять роль",
     )
     @commands.has_permissions(administrator=True, manage_roles=True)
-    async def getrole(self, inter, member: disnake.Member, role: disnake.Role):
-        self.logger.info(f"Попытка назначения {role.name} участнику {member.display_name}.")
+    async def role(
+        self, 
+        inter: disnake.GuildCommandInteraction,
+        member: disnake.Member, 
+        role: disnake.Role,        
+        action: roleMenegment,
+    ):
+        """
+            Назначение роли участнику сервера
+
+            Parameters
+            ----------
+            member: Выберите пользователя для назначения роли
+            role: Выберите роль для назначения на пользователя
+            action: Назначить или снять роль
+        """
         try:
-            await member.add_roles(role)
-            await inter.response.send_message(f"Роль {role.name} успешно добавлена участнику {member.display_name}.")
-            self.logger.info(f"Роль {role.name} успешно добавлена участнику {member.display_name}.")
+            if action == 'add':
+                await member.add_roles(role)
+                await inter.response.send_message(f"Роль {role.name} успешно добавлена участнику {member.display_name}.")
+                self.logger.info(f"Роль {role.name} успешно добавлена участнику {member.display_name}.")
+            else:
+                await member.remove_roles(role)
+                await inter.response.send_message(f"Роль {role.name} успешно снята с участника {member.display_name}.")
+                self.logger.info(f"Снятие роли {role.name} с участника {member.display_name}.")
         except disnake.errors.Forbidden:
             await inter.response.send_message("У меня нет прав для изменения ролей.")
             self.logger.info(f"Ошибка: недостаточно прав")
-        except disnake.errors.HTTPException as e:
+        except Exception as e:
             await inter.response.send_message(f"Произошла ошибка")
             self.logger.info(f"Произошла ошибка: {e}")
 
+    contestStatus = commands.option_enum({"Запуск конкурса": "start", "Завершение конкурса": "stop"})    
     @commands.slash_command(
         name="contest",
         description="Организация конкурса",
     )
-    @commands.has_permissions(administrator=True)  # Можно изменить на более подходящие права
-    async def addreaction(self, inter, channel: disnake.TextChannel, emoji: disnake.Emoji):
-        guild_id = inter.guild.id
-        channel_id = channel.id
-        emoji_str = str(emoji)
-        
-        print(guild_id, channel_id, emoji_str)
-        await inter.response.send_message(f'{guild_id}, {channel_id}, {emoji_str}')
+    @commands.has_permissions(administrator=True)
+    async def contest(
+        self,
+        inter: disnake.GuildCommandInteraction,
+        channel: disnake.TextChannel,
+        emoji: disnake.Emoji,
+        status: contestStatus,        
+    ):
+        """
+            Создание и завершение конкурса
+
+            Parameters
+            ----------
+            channel: Выбор канала ,в котором будет проводться или завершаться конкурс
+            emoji: Эмодзи, которая будет автоматически проставляться на новых сообщениях
+            status: Параметр, управляющий стартом и завершением конкурса
+        """
+        try:
+            guild_id = inter.guild.id
+            channel_id = channel.id
+            emoji_str = str(emoji)
+            
+            print(guild_id, channel_id, emoji_str, status)
+            await inter.response.send_message(f'{guild_id}, {channel_id}, {emoji_str}, {status}')
+        except Exception as e:
+            print(f'Ошибка в commands/contest: {e}')
+            
 
 def setup(bot, logger):
     bot.add_cog(SlashCommands(bot, logger))
