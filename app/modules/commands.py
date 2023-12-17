ceb980d20f1c8f051397a68b56687be279ce6d75
Tomich37 <tomich7372@gmail.com>
2023-12-17 19:16:01 +0700

    Добавлен зачаток help

--- app/modules/commands.py
+++ app/modules/commands.py
@@ -11,6 +11,36 @@ class SlashCommands(commands.Cog):
         self.sc = Scripts(logger, bot)
 
     @commands.slash_command(
+        name='help',
+        description='Помощь по боту'
+    )
+    async def help(
+        self,
+        inter: disnake.GuildCommandInteraction,
+    ):
+        """
+            Назначение роли участнику сервера
+        """
+        embed = disnake.Embed(
+            title=f"Помощь по командам бота",
+            description="",
+            color=0x00ff00
+        )
+        embed.add_field(
+            name="Модерация",
+            value="""**contest** - организация конкурса в поределенном канале
+            **role** - назначить / снять роль с участника""",
+            inline=False
+        )
+        embed.add_field(
+            name="Другое",
+            value="""**ping** - пинг бота""",
+            inline=False
+        )
+        embed.set_footer(text=f'Made by the_usual_god')
+        await inter.response.send_message(embed=embed)
+
+    @commands.slash_command(
         name="ping",
         description="Возвращает задержку бота",
     )
